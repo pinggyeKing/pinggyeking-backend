555dd22ee98415a1b8ec3557c8f808eed08c2d29
강민혁 <rkdglqkr12@naver.com>
2025-07-03 09:43:25 +0900

    feat: ㅁì기본 설정 세팅

---
+++ src/main/java/com/swyp10/pinggyewang/PinggyeWangApplication.java
@@ -0,0 +1,13 @@
+package com.swyp10.pinggyewang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinggyeWangApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PinggyeWangApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/swyp10/pinggyewang/PinggyeWangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swyp10.pinggyewang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinggyeWangApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
